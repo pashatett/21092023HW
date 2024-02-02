bba40f2d580f23043d8a91ff22df1474e63585f9
pashka <pashatett1@mail.ru>
2024-02-02 16:31:01 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Car.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarReacing
+{
+    internal class Car
+    {
+        public string name;
+        public int maxBoost;
+        public int distance;
+        public double unitOfCalculation;//выдуманные единицы измерения для сравнения машин
+
+        public Random random = new Random();
+
+        public Car(string name, int maxBoost)
+        {
+            this.name = name;
+            this.maxBoost = maxBoost;
+        }
+        public virtual void Racing()
+        {
+            while (distance < 100)
+            {
+                maxBoost += random.Next(1, maxBoost);
+                distance += maxBoost;
+            }
+            unitOfCalculation = (double)distance / (double)maxBoost;
+        }
+        public virtual void Finish()
+        {
+            Console.WriteLine($"Победил {name}");
+            return;
+        }
+    }
+    class SportCar : Car
+    {
+        public SportCar(string name, int maxBoost) : base(name, maxBoost) { }
+        public override void Racing()
+        {
+            base.Racing();
+        }
+        public override void Finish()
+        {
+            base.Finish();
+        }
+    }
+
+    class Bus : Car
+    {
+        public Bus(string name, int maxBoost) : base(name, maxBoost) { }
+        public override void Racing()
+        {
+            base.Racing();
+        }
+        public override void Finish()
+        {
+            base.Finish();
+        }
+    }
+
+    class Passeger : Car
+    {
+        public Passeger(string name, int maxBoost) : base(name, maxBoost) { }
+        public override void Racing()
+        {
+            base.Racing();
+        }
+        public override void Finish()
+        {
+            base.Finish();
+        }
+    }
+
+    class Track : Car
+    {
+        public Track(string name, int maxBoost) : base(name, maxBoost) { }
+        public override void Racing()
+        {
+            base.Racing();
+        }
+        public override void Finish()
+        {
+            base.Finish();
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarReacing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SportCar sportCar = new SportCar("Sportcar", 10);
+            sportCar.Racing();
+            Passeger passeger = new Passeger("Car", 8);
+            passeger.Racing();
+            Bus bus = new Bus("Bus", 6);
+            bus.Racing();
+            Track track = new Track("Gruzovik", 4);
+            track.Racing();
+
+            if (sportCar.unitOfCalculation < passeger.unitOfCalculation && sportCar.unitOfCalculation < bus.unitOfCalculation && sportCar.unitOfCalculation < track.unitOfCalculation)
+            {
+                sportCar.Finish();
+            }
+
+            if (passeger.unitOfCalculation < sportCar.unitOfCalculation && passeger.unitOfCalculation < bus.unitOfCalculation && passeger.unitOfCalculation < track.unitOfCalculation)
+            {
+                passeger.Finish();
+            }
+
+            if (bus.unitOfCalculation < passeger.unitOfCalculation && bus.unitOfCalculation < sportCar.unitOfCalculation && bus.unitOfCalculation < track.unitOfCalculation)
+            {
+                bus.Finish();
+            }
+
+            if (track.unitOfCalculation < passeger.unitOfCalculation && track.unitOfCalculation < bus.unitOfCalculation && track.unitOfCalculation < sportCar.unitOfCalculation)
+            {
+                track.Finish();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
